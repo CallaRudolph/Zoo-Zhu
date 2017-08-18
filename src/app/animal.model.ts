33ef2c055150c276d57e74a679642f16c087b16b
Calla Rudolph <callarudolph@gmail.com>
2017-08-18 09:10:56 -0700

    hard code several animals in master list

--- src/app/animal.model.ts
+++ src/app/animal.model.ts
@@ -1,3 +1,3 @@
 export class Animal {
-  constructor(public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) { }
+  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) { }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,11 @@ import { Animal } from './animal.model';
 
 export class AppComponent {
   title = 'Welcome to Zoo Zhu!!!';
+
+  masterAnimalList: Animal[] = [
+    new Animal('Flamingo', 'MuShu', 3, 'Omnivore', 'Lagoon', 2, 'Female', 'Standing on one leg', 'Being alone'),
+    new Animal('Polar Bear', 'Salty', 13, 'Carnivore', 'Glacier', 4, 'Male', 'Eating the flesh of seals', 'Drunk bantering humans'),
+    new Animal('Giraffe', 'Stalk', 1, 'Herbivore', 'Savanna', 6, 'Male', 'Open plains', 'Lions, Leopards, Hyenas'),
+    new Animal('Meerkat', 'G-Unit', 2, 'Carnivore', 'Desert Burrows', 3, 'Female', 'Scorpions, Socializing', 'Sitting')
+  ]
 }
