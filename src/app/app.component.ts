8a170b636596e9b76fe1b986c51820344e3a1399
Calla Rudolph <callarudolph@gmail.com>
2017-08-18 15:19:54 -0700

    hard code two more animals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,9 @@ export class AppComponent {
     new Animal('Flamingo', 'MuShu', 3, 'Omnivore', 'Lagoon', 2, 'Female', 'Standing on one leg', 'Being alone'),
     new Animal('Polar Bear', 'Salty', 13, 'Carnivore', 'Glacier', 4, 'Male', 'Eating the flesh of seals', 'Drunk bantering humans'),
     new Animal('Giraffe', 'Stalk', 1, 'Herbivore', 'Savanna', 6, 'Male', 'Open plains', 'Lions, Leopards, Hyenas'),
-    new Animal('Meerkat', 'G-Unit', 2, 'Carnivore', 'Desert Burrows', 3, 'Female', 'Scorpions, Socializing', 'Sitting')
+    new Animal('Meerkat', 'G-Unit', 2, 'Carnivore', 'Desert Burrows', 3, 'Female', 'Scorpions, Socializing', 'Sitting'),
+    new Animal('Bullet Ant', 'Pantsy', 1, 'Herbivore', 'Rainforest', 0, 'Male', 'Stinging Intruders', 'Starvation'),
+    new Animal('Western Chuckwalla', 'Tilda', 25, 'Omnivore', 'Rocky Desert', 3, 'Female', 'Being pot-bellied and baggy', 'Having a scientific name of obesus obesus')
   ];
 
   addAnimal(newAnimalfromChild: Animal) {
