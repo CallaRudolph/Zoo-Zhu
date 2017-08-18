cc74a92bb845e4b0121464f3607d4db6a7b9a458
Calla Rudolph <callarudolph@gmail.com>
2017-08-18 08:55:46 -0700

    create animal model with constructor

---
+++ src/app/animal.model.ts
@@ -0,0 +1,3 @@
+export class Animal {
+  constructor(public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) { }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Welcome to Zoo Zhu!!!';
 }
